620aada696c12d4b75a833f1747270e3d8cf6b67
ghhkmf <gh_hakimifard@yahoo.com>
2018-06-16 09:48:46 +0200

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,10 +1,14 @@
 """Model definitions (one class per file) to define NN architectures."""
-from .bayes import NaiveBayes
+#from .bayes import NaiveBayes
+from .svm import Svm
 
-__all__ = ('NaiveBayes', )
+#__all__ = ('NaiveBayes', )
+__all__ = ('Svm', )
 
 def getModel(name):
     if name == "NaiveBayes":
         return NaiveBayes
+    elif name == "Svm":
+        return Svm
     else:
         raise NotImplementedError("No model called \"%s\"!" % name)

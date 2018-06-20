9c549efffa3ed11d7d578ae4c2d96b59143f4c34
acDante <shana0123@163.com>
2018-06-20 23:23:32 +0800

    split main into two files

--- src/main.py
+++ src/main.py
@@ -1,71 +1,33 @@
-#!/usr/bin/env python3
-import os
-import sys
-import models
-import datetime
-import utils
+# Read the embedding vectors from file and train the classifier
+
+# This file is still under construction.. TODO 
+
 import numpy as np
-from sklearn.model_selection import train_test_split
-from gensim.models import KeyedVectors
-from datasources import feature_extraction
 
 pos_default = "../data/twitter-datasets/train_pos.txt"
 neg_default = "../data/twitter-datasets/train_neg.txt"
 test_default = "../data/twitter-datasets/test_data.txt"
-embedding_file = "./datasources/word2vec_embedding.txt"
+preprocessed_data = "./preprocessed.npz"
 
-if __name__ == "__main__":
-    if not os.getcwd().endswith("/src"):
-        os.chdir("src")
+data = np.load(preprocessed_data)
 
-    argc = len(sys.argv)
+# 3. split dataset to training set and validation set
+X_train, X_val, y_train, y_val = train_test_split(data['X'], data['y'], test_size=0.33, random_state=42)
 
-    # print usage
-    if argc < 2 or sys.argv[1] not in models.__all__:
-        print("Usage: python3.6 main.py [model] [path1] [path2] [path3]")
-        print("    Available models: " + str(models.__all__))
-        print("    Path to positive training dataset (path1): default is " + pos_default)
-        print("    Path to negative training dataset (path2): default is " + neg_default)
-        print("    Path to test dataset (path3): default is " + test_default)
-    else:
-        pos_src = pos_default if argc < 3 else sys.argv[2]
-        neg_src = neg_default if argc < 4 else sys.argv[3]
-        test_src = test_default if argc < 5 else sys.argv[4]
-        
-        ### general pipeline here 
-        # 1. generate word embedding
-        word_vectors = KeyedVectors.load_word2vec_format(embedding_file)
-        
-        # 2. generate document embedding and build the label vector
-        X = average_vector(text_files=[pos_default, neg_default], embedding=word_vectors)   # need to add import for this
-        y = np.array([])   
-        for fn in [pos_default, neg_default]:
-            with open(fn) as f:
-                lines = f.readlines()
-                n_lines = len(lines)
-                if fn == pos_default:
-                    y = np.full((n_lines, 1), 1)
-                else:
-                    labels = np.full((n_lines, 1), -1)
-                    y = np.vstack((y, labels))
-        
-        # 3. split dataset to training set and validation set
-        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.33, random_state=42)
+# Train the model (need to be modified) TODO
+print("Training " + sys.argv[1] + "model on positive dataset " + pos_default +
+      " and negative dataset " + neg_default)
+model = models.getModel(sys.argv[1])()
+model.train(X_train, y_train)
 
-        # Train the model
-        print("Training " + sys.argv[1] + "model on positive dataset " + pos_src +
-              " and negative dataset " + neg_src)
-        model = models.getModel(sys.argv[1])()
-        model.train(X_train, y_train)
-
-        # 4. compute and output the validation error [TO DO]
+# 4. compute and output the validation error [TODO]
 
 
-        # 5. Preprocess test data into feature vectors [TO DO]
+# 5. Preprocess test data into feature vectors [TODO]
 
 
-        # Evaluate for Kaggle submission (need to be modified)
-        print("Evaluating for kaggle submission on test set " + test_src)
-        utils.ensure_dir("../output/models/%s" % sys.argv[1])
-        model.evaluate_for_kaggle(test_src,
-                                  "../output/models/%s/kaggle_%s.csv" % (sys.argv[1], datetime.datetime.now().strftime("%Y%m%d%H%M%S")))
+# Evaluate for Kaggle submission (need to be modified) TODO
+print("Evaluating for kaggle submission on test set " + test_default)
+      utils.ensure_dir("../output/models/%s" % sys.argv[1])
+model.evaluate_for_kaggle(test_default,
+                          "../output/models/%s/kaggle_%s.csv" % (sys.argv[1], datetime.datetime.now().strftime("%Y%m%d%H%M%S")))
